e74f277d36e16d8c59006f09daa2c6e0983101a4
nevilparmar11 <nevilparmar24@gmail.com>
2021-01-12 01:17:53 +0530

    code cleanup + adding temp home page to the site

--- onlineassessmentsystem/onlineassessmentsystem/urls.py
+++ onlineassessmentsystem/onlineassessmentsystem/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from users import views as user_views
 
 urlpatterns = [
+    path('', user_views.index),
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
 ]
--- onlineassessmentsystem/users/views.py
+++ onlineassessmentsystem/users/views.py
@@ -1,34 +1,48 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from .models import User
+
+'''
+Function to show homepage of the site
+'''
+
+
+def index(request):
+    return render(request, 'index.html')
+
 
 '''
 Function to authenticate a user.
 '''
+
+
 def loginUser(request):
-    if request.method=="GET":
-        return render(request,"users/login.html")
+    if request.method == "GET":
+        return render(request, "users/login.html")
     else:
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(username=username,password=password)
+        user = authenticate(username=username, password=password)
         if user is not None:
-            login(request,user)
-            return redirect('/users/home')  
+            login(request, user)
+            return redirect('/users/home')
         else:
-            return render(request,"users/login.html",
-                            {
-                                "errorMessage":"Invalid Credentials",
-                                "username" : username,
-                                "password" : password
-                            }
-                        )
+            return render(request, "users/login.html",
+                          {
+                              "errorMessage": "Invalid Credentials",
+                              "username": username,
+                              "password": password
+                          }
+                          )
+
 
 '''
 Function to redirect user to home page
 '''
+
+
 def home(request):
     if request.user.isStudent:
-        return render(request,'users/studentDashboard.html')
+        return render(request, 'users/studentDashboard.html')
     else:
-        return render(request,'users/facultyDashboard.html')
+        return render(request, 'users/facultyDashboard.html')
