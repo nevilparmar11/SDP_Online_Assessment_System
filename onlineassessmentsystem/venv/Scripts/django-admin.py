7ab4914841175a1221fbd7ea61841e2a9b8d543e
nevilparmar11 <nevilparmar24@gmail.com>
2021-01-19 00:36:40 +0530

    configuring env

--- onlineassessmentsystem/venv/Scripts/django-admin.py
+++ onlineassessmentsystem/venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!F:\Ayan\Sem6\SDP\Project\SDP_Online_Assessment_System\onlineassessmentsystem\venv\Scripts\python.exe
+#!C:\Users\nevil\PycharmProjects\onlineassessmentsystem\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
