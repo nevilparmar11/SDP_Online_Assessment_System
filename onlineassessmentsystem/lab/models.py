f314f405cddd5568f60ab4602220b590dddaa3bf
ayanshaikh18 <mahammadayan18@gmail.com>
2021-02-19 21:40:52 +0530

    LabGrade Table added & Lab grading implemented

--- onlineassessmentsystem/lab/models.py
+++ onlineassessmentsystem/lab/models.py
@@ -1,5 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
+from django_q.models import Schedule
+
 from classroom.models import Classroom
+from users.models import User
 
 
 # Create your models here.
@@ -11,3 +15,31 @@ class Lab(models.Model):
     subject = models.CharField(null=False, max_length=50, default="DEFAULT-SUBJECT")
     description = models.CharField(null=False, max_length=1000, default="Default Lab description")
     deadline = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        print(self.labId)
+        try:
+            schedule = Schedule.objects.get(name=str(self.labId))
+            schedule.next_run = self.deadline
+            schedule.repeats = 1
+            schedule.save()
+            print("Schedule Updated for lab :- " + self.title)
+
+        except ObjectDoesNotExist:
+            Schedule.objects.create(
+                name=str(self.labId),
+                func="lab.tasks.performLabGrading",
+                args=str(self.labId),
+                schedule_type='O',
+                repeats=1,
+                next_run=self.deadline,
+            )
+            print("Schedule added for lab :- " + self.title)
+
+
+class LabGrade(models.Model):
+    labGradeId = models.AutoField(primary_key=True)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    lab = models.ForeignKey(Lab, on_delete=models.CASCADE)
+    grade = models.CharField(max_length=2, null=False, default='F')
